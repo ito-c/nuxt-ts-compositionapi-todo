f4718d71645e72c475ee97a21912d0ec9e28d6a6
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-04-11 21:30:10 +0900

    タスク一覧

---
+++ components/TaskList.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="task-list-wrapper">
+    <div>
+      <input v-model="state.taskName" type="text" />
+      <button @click="addTask">タスク追加</button>
+    </div>
+    <div><input v-model="state.searchText" type="text" />Search</div>
+    <div class="task-list">
+      <ul>
+        <h3>Doing</h3>
+        <li v-for="(task, index) in state.doingTasks" :key="index">
+          <input type="checkbox" :checked="task.status" disabled />
+          <label>{{ task.name }}</label>
+          <button @click="toggleTask(task, true)">toggle</button>
+        </li>
+      </ul>
+      <ul>
+        <h3>Completed</h3>
+        <li v-for="(task, index) in completedTasks" :key="index">
+          <input type="checkbox" :checked="tasl.status" disabled />
+          <label>{{ task.name }}</label>
+          <button @click="toggleTask(task, false)">toggle</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, reactive } from '@nuxtjs/composition-api'
+
+interface Task {
+  tasks: {
+    taskName: string
+    status: boolean
+  }
+}
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      taskName: '',
+      searchText: '',
+      tasks: [],
+      doingTasks: computed(() =>
+        state.searchedTasks.filter((t: Task) => !t.status)
+      ),
+      completedTasks: computed(() =>
+        state.searchedTasks.filter((t: Task) => t.status)
+      ),
+      searchedTasks: computed(() =>
+        state.tasks.filter((t: Task) => t.name.includes(state.searchtext))
+      ),
+    })
+
+    const addTask = () => {
+      state.tasks.push({
+        name: state.taskName,
+        status: false,
+      })
+      state.taskName = ''
+    }
+
+    const toggleTask = (task, status) => {
+      const index = state.tasks.indexOf(task)
+      state.tasks.splice(index, 1, { ...task, status })
+    }
+
+    return {
+      state,
+      addTask,
+      toggleTask,
+    }
+  },
+})
+</script>
+
+<style scoped>
+.task-list-wrapper {
+  border-top: 1px solid gray;
+}
+.task-list {
+  display: flex;
+  justify-content: center;
+}
+</style>
