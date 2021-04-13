1fe766f8e52addbf4c3a277cd626ca0fa770a13d
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-04-14 00:18:24 +0900

    Formコンポーネント作成と開通

---
+++ components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>form component</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const state = reactive({})
+    return {
+      state,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/form/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>test</div>
+    <Form />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@nuxtjs/composition-api'
+import Form from '~/components/Form.vue'
+
+export default defineComponent({
+  components: {
+    Form,
+  },
+
+  setup() {
+    const state = reactive({})
+    return {
+      state,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
