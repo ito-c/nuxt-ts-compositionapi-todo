a72f67074cff63ca7fac83f81ad7ec81532367eb
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-05-05 23:47:59 +0900

    状態の更新（sync使わない）

---
+++ components/FormPlaySync.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="FormPlaySync">
+    <h2 class="title">FormPlaySync</h2>
+    <el-input :value="props.formData" @input="updateFormData"></el-input>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    formData: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+
+  setup(props, { emit }) {
+    const updateFormData = (input: string) => {
+      emit('updateFormData', input)
+    }
+
+    return {
+      props,
+      updateFormData,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/form/play-ground.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="FormPlayGroundPage">
+    <h1 class="title">FormPlayGroundPage</h1>
+    <FormPlaySync
+      :form-data="state.formData"
+      @updateFormData="updateFormData"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@nuxtjs/composition-api'
+import FormPlaySync from '~/components/FormPlaySync.vue'
+
+export default defineComponent({
+  components: {
+    FormPlaySync,
+  },
+
+  setup() {
+    const state = reactive({
+      formData: '',
+    })
+
+    const updateFormData = (input: string) => {
+      state.formData = input
+    }
+
+    return {
+      state,
+      updateFormData,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
