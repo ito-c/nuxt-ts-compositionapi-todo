f17d081c96983540b2a34916145c01c006452d6d
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-05-09 22:19:47 +0900

    axiosのテストを再現する

---
+++ pages/form/axios.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>axios</h1>
+    <el-button round @click="axiosMethod">button</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useContext } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const { $axios } = useContext()
+
+    const axiosMethod = async () => {
+      await $axios.post('api/hoge')
+    }
+
+    return {
+      axiosMethod,
+    }
+  },
+})
+</script>
+
+<style></style>
